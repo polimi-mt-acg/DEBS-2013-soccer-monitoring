df902753dddfcf835d0850f6371f1d219e394a80
Leonardo Arcari <lippol94@gmail.com>
2018-07-06 12:21:12 +0200

    Update CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
 #include <iostream>
 
-int main(int, char**) {
-   std::cout << "Hello, world!\n";
-}
+int main(int, char **) { std::cout << "Hello, world!\n"; }
--- test/hello.cpp
+++
@@ -1,3 +0,0 @@
-#include "catch.hpp"
-
-TEST_CASE("Testing a test case") { REQUIRE(true == true); }
