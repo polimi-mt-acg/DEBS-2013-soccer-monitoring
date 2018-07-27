497bc43bbe23904f99f61b31b7d49df32078b6e9
Leonardo Arcari <lippol94@gmail.com>
2018-07-27 12:10:21 +0200

    Add Position classes to track sensors position
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

---
+++ include/position.hpp
@@ -0,0 +1,88 @@
+#ifndef GAME_POSITION_H
+#define GAME_POSITION_H
+
+#include <array>
+#include <numeric>
+#include <tuple>
+#include <variant>
+#include <vector>
+
+namespace game {
+template <typename Derived> class Position {
+public:
+  std::tuple<float, float, float> vector() const {
+    return static_cast<Derived &>(*this).vector();
+  }
+
+  void add_sensor(int sid) { static_cast<Derived &>(*this).add_sensor(sid); }
+
+  void update_sensor(int sid, std::tuple<int, int, int> vector) {
+    static_cast<Derived &>(*this).update_sensor(sid, vector);
+  }
+
+  std::vector<int> get_sids() const {
+    return static_cast<Derived &>(*this).get_sids();
+  }
+};
+
+class BallPosition : public Position<BallPosition> {
+public:
+  std::tuple<float, float, float> vector() const {
+    return {xs[game_ball], ys[game_ball], zs[game_ball]};
+  }
+
+  void update_sensor(int sid, std::tuple<int, int, int> vector);
+
+  void add_sensor(int sid) {
+    sids.push_back(sid);
+    xs.push_back(0);
+    ys.push_back(0);
+    zs.push_back(0);
+
+    game_ball = sids.size() - 1; // Set last add ball as the game one
+  }
+
+  std::vector<int> get_sids() const { return sids; }
+
+private:
+  std::size_t game_ball = -1;
+  std::vector<int> sids = {};
+  std::vector<int> xs = {};
+  std::vector<int> ys = {};
+  std::vector<int> zs = {};
+
+  std::size_t sid_index(int sid) const;
+};
+
+class PlayerPosition : public Position<PlayerPosition> {
+public:
+  std::tuple<float, float, float> vector() const;
+  void update_sensor(int sid, std::tuple<int, int, int> vector);
+  void add_sensor(int sid) {
+    sids.push_back(sid);
+    xs.push_back(0);
+    ys.push_back(0);
+    zs.push_back(0);
+  }
+  std::vector<int> get_sids() const { return sids; };
+
+private:
+  std::vector<int> sids = {};
+  std::vector<int> xs = {};
+  std::vector<int> ys = {};
+  std::vector<int> zs = {};
+
+  std::size_t sid_index(int sid) const;
+};
+
+using Positions = std::variant<BallPosition, PlayerPosition>;
+
+template <typename Iter> float mean(Iter first, Iter end) {
+  auto sum = std::accumulate(first, end, 0.0f);
+  auto n = std::distance(first, end);
+  return static_cast<float>(sum) / n;
+}
+
+} // namespace game
+
+#endif
---
+++ src/position.cpp
@@ -0,0 +1,66 @@
+#include "position.hpp"
+#include "event.hpp"
+
+#include <algorithm>
+#include <exception>
+#include <position.hpp>
+
+#include "fmt/format.h"
+
+namespace game {
+
+// ==-----------------------------------------------------------------------==
+//                        PlayerPosition implementation
+// ==-----------------------------------------------------------------------==
+
+void PlayerPosition::update_sensor(int sid, std::tuple<int, int, int> vector) {
+  auto [x, y, z] = vector;
+  auto idx = sid_index(sid);
+  xs[idx] = x;
+  ys[idx] = y;
+  zs[idx] = z;
+}
+
+std::size_t PlayerPosition::sid_index(int sid) const {
+  if (auto it = std::find(sids.cbegin(), sids.cend(), sid); it != sids.cend()) {
+    return std::distance(sids.cbegin(), it);
+  } else {
+    throw std::out_of_range{fmt::format("Unknown sid {}", sid)};
+  }
+}
+
+std::tuple<float, float, float> PlayerPosition::vector() const {
+  auto x_bar = mean(xs.cbegin(), xs.cend());
+  auto y_bar = mean(ys.cbegin(), ys.cend());
+  auto z_bar = mean(zs.cbegin(), zs.cend());
+
+  return {x_bar, y_bar, z_bar};
+}
+
+// ==-----------------------------------------------------------------------==
+//                        BallPosition implementation
+// ==-----------------------------------------------------------------------==
+
+std::size_t BallPosition::sid_index(int sid) const {
+  if (auto it = std::find(sids.cbegin(), sids.cend(), sid); it != sids.cend()) {
+    return std::distance(sids.cbegin(), it);
+  } else {
+    throw std::out_of_range{fmt::format("Unknown sid {}", sid)};
+  }
+}
+
+void BallPosition::update_sensor(int sid, std::tuple<int, int, int> vector) {
+  // Update sensor position
+  auto [x, y, z] = vector;
+  auto idx = sid_index(sid);
+  xs[idx] = x;
+  ys[idx] = y;
+  zs[idx] = z;
+
+  // If sensor inside the field, set as game ball
+  if (field_lower_x <= x && x <= field_upper_x && field_lower_y <= y &&
+      y <= field_upper_y) {
+    game_ball = idx;
+  }
+}
+} // namespace game
