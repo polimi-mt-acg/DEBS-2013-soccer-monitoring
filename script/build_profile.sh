34d4ab4ded5912e180d0eb6ca1b9ea4f0be0b66e
Leonardo Arcari <lippol94@gmail.com>
2018-08-09 22:43:10 +0200

    Add build_profile.sh script to correctly build for code profiling
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

---
+++ script/build_profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Compute paths
+WORKING_DIR=`pwd`
+APPLICATION_ROOT=`git rev-parse --show-toplevel`
+
+# First clean the build folder (if any)
+if [ -d "$APPLICATION_ROOT/cmake-build-profile" ]; then
+    rm -rf $APPLICATION_ROOT/cmake-build-profile
+fi
+
+# Compile application
+mkdir -p $APPLICATION_ROOT/cmake-build-profile
+cd $APPLICATION_ROOT/cmake-build-profile
+cmake -DPERF_PROFILING=ON -DCMAKE_BUILD_TYPE=Release ..
+make
+
