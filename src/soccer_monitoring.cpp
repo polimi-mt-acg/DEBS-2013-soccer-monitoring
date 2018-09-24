2c91f8b61ed2f90149f1f1fbdf6e0a4e2e8e85e3
Leonardo Arcari <lippol94@gmail.com>
2018-09-24 14:34:54 +0200

    Refactor and document undocumented headers
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

--- src/soccer_monitoring.cpp
+++ src/soccer_monitoring.cpp
@@ -8,42 +8,16 @@
 #include <soccer_monitoring.hpp>
 
 namespace game {
-
 void run_game_monitoring(int time_units, double maximum_distance,
                          std::filesystem::path const &game_data,
                          std::filesystem::path const &metadata, int nb_threads,
                          std::size_t batch_size) {
-  omp_set_num_threads(nb_threads);
   auto context = game::Context::build_from(metadata);
-
-  //= ----------------------- Business logic -------------------------------- =
   auto visualizer =
       game::Visualizer{context.get_players(), context.get_teams(), time_units};
-  auto fetcher = game::EventFetcher{game_data.string(), game::file_stream{},
-                                    time_units, batch_size, context};
-  auto stats = game::GameStatistics{maximum_distance, context};
 
-  visualizer.draw();
-  auto t1 = std::chrono::steady_clock::now();
-  for (auto const &batch : fetcher) {
-    // Check if batch returned because time_units seconds are elapsed
-    stats.accumulate_stats(batch);
-
-    if (batch.is_period_last_batch) {
-      auto const &partials = stats.last_partial();
-
-      auto t2 = std::chrono::steady_clock::now();
-      std::chrono::duration<double> diff = t2 - t1;
-      fmt::print("Processed {} seconds of the stream (~ {} events) in {:.3f} "
-                 "seconds\n",
-                 time_units, time_units * 1500, diff.count());
-      t1 = t2;
-      visualizer.draw_stats(partials, false, batch.final_ts);
-    }
-  }
-
-  auto game_stats = stats.game_stats();
-  visualizer.draw_final_stats(game_stats);
+  details::run_game_monitoring(time_units, maximum_distance, game_data,
+                               nb_threads, batch_size, context, visualizer);
 }
 
 void run_game_monitoring(int time_units, double maximum_distance,
@@ -51,15 +25,23 @@ void run_game_monitoring(int time_units, double maximum_distance,
                          std::filesystem::path const &metadata, int nb_threads,
                          std::size_t batch_size,
                          std::string const &output_path) {
-  omp_set_num_threads(nb_threads);
   auto context = game::Context::build_from(metadata);
-
-  //= ----------------------- Business logic -------------------------------- =
   auto visualizer = game::Visualizer{context.get_players(), context.get_teams(),
                                      time_units, output_path};
+
+  details::run_game_monitoring(time_units, maximum_distance, game_data,
+                               nb_threads, batch_size, context, visualizer);
+}
+
+namespace details {
+void run_game_monitoring(int time_units, double maximum_distance,
+                         std::filesystem::path const &game_data, int nb_threads,
+                         std::size_t batch_size, Context &context,
+                         Visualizer &visualizer) {
   auto fetcher = game::EventFetcher{game_data.string(), game::file_stream{},
                                     time_units, batch_size, context};
   auto stats = game::GameStatistics{maximum_distance, context};
+  omp_set_num_threads(nb_threads);
 
   visualizer.draw();
   auto t1 = std::chrono::steady_clock::now();
@@ -74,7 +56,7 @@ void run_game_monitoring(int time_units, double maximum_distance,
       std::chrono::duration<double> diff = t2 - t1;
       fmt::print("Processed {} seconds of the stream (~ {} events) in {:.3f} "
                  "seconds\n",
-                 time_units, time_units * 1500, diff.count());
+                 time_units, time_units * 15000, diff.count());
       t1 = t2;
       visualizer.draw_stats(partials, false, batch.final_ts);
     }
@@ -83,4 +65,5 @@ void run_game_monitoring(int time_units, double maximum_distance,
   auto game_stats = stats.game_stats();
   visualizer.draw_final_stats(game_stats);
 }
+} // namespace details
 } // namespace game
